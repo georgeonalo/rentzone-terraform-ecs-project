1942924c0091f4c7dcf576062b86f851b8cab194
George Onalo <Onalotech7@gmail.com>
2023-03-23 10:28:31 +0100

    created terraform files

---
+++ terrform.tfvars
@@ -0,0 +1,4 @@
+# enviroment variables
+region="us-east-1"
+project_name="rentzone"
+environment="dev"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+ # enviroment variables
+ variable "region" {
+    description = "region to create resources"
+    type        = string
+ }
+
+ variable "project_name" {
+    description = "project name"
+    type        = string
+ }
+
+ variable "environment" {
+    description = "environment"
+    type        = string
+ }
